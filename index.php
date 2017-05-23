a02a742b1ef60423198a4ddfc0eb4b4e9cbec940
Darkbelg <stijn.sagaert@outlook.com>
2017-05-23 15:43:09 +0200

    Basic partfolio met bootstrap template

--- index.php
+++
@@ -1,8 +0,0 @@
-<?php
-require('vendor/autoload.php');
-Twig_Autoloader::register();
-$loader = new Twig_Loader_Filesystem("Presentation");
-$twig = new Twig_Environment($loader);
-$twigArray["boodschap"] = "Hello world";
-$view = $twig->render("index.twig",$twigArray);
-print $view;
